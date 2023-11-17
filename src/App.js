9c875227623052c6511a69542ffdcb76b8cb8414
rafsun-riad <mruhaquer@gmail.com>
2023-11-18 00:46:59 +0600

    done some clean up & add local storage

--- src/App.js
+++ src/App.js
@@ -1,51 +1,6 @@
 import { useEffect, useState } from 'react';
 import StarRating from './StarRating';
 
-// const tempMovieData = [
-// 	{
-// 		imdbID: 'tt1375666',
-// 		Title: 'Inception',
-// 		Year: '2010',
-// 		Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-// 	},
-// 	{
-// 		imdbID: 'tt0133093',
-// 		Title: 'The Matrix',
-// 		Year: '1999',
-// 		Poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-// 	},
-// 	{
-// 		imdbID: 'tt6751668',
-// 		Title: 'Parasite',
-// 		Year: '2019',
-// 		Poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-// 	},
-// ];
-
-// const tempWatchedData = [
-// 	{
-// 		imdbID: 'tt1375666',
-// 		Title: 'Inception',
-// 		Year: '2010',
-// 		Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-// 		runtime: 148,
-// 		imdbRating: 8.8,
-// 		userRating: 10,
-// 	},
-// 	{
-// 		imdbID: 'tt0088763',
-// 		Title: 'Back to the Future',
-// 		Year: '1985',
-// 		Poster: 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-// 		runtime: 116,
-// 		imdbRating: 8.5,
-// 		userRating: 9,
-// 	},
-// ];
-
-// http://www.omdbapi.com/?i=tt3896198&apikey=fb2195c1
-// http://www.omdbapi.com/?apikey=${KEY}&s=intersteller
-
 const KEY = 'fb2195c1';
 
 const average = (arr) =>
@@ -53,11 +8,16 @@ const average = (arr) =>
 
 export default function App() {
 	const [movies, setMovies] = useState([]);
-	const [watched, setWatched] = useState([]);
+	// const [watched, setWatched] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState('');
 	const [query, setQuery] = useState('');
 	const [selectedId, setSelectedId] = useState(null);
+
+	const [watched, setWatched] = useState(() => {
+		const storedValue = localStorage.getItem('watched');
+		return JSON.parse(storedValue);
+	});
 
 	function handleSelectMovie(id) {
 		setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -69,11 +29,17 @@ export default function App() {
 
 	function handleAddWatched(movie) {
 		setWatched((watched) => [...watched, movie]);
+
+		// localStorage.setItem('watched', JSON.stringify([...watched, movie]));
 	}
 
 	function handleDeleteWatched(id) {
 		setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
 	}
+
+	useEffect(() => {
+		localStorage.setItem('watched', JSON.stringify(watched));
+	}, [watched]);
 
 	useEffect(
 		function () {
